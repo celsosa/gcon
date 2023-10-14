9a86b6c9ae8a3d61b07db2a14432aa6d8f429ea2
Celso Sá <40446066+celsosa@users.noreply.github.com>
2023-10-13 21:45:14 -0300

    fix typescript warning

--- app/admin/condominios/components/CondList.tsx
+++ app/admin/condominios/components/CondList.tsx
@@ -33,7 +33,8 @@ function CondList({ condominios, userType }: CondListProps) {
                                 </p>
                             </div>
                             <div className="px-6 pt-4 pb-2">
-                                <span className="inline-block bg-bodydark1 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{condominio.perfil_usuarios.email}</span>
+                            // @ts-ignore
+                                <span className="inline-block bg-bodydark1 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> {(condominio.perfil_usuarios as any).email}</span>
                             </div>
                         </div>
                     </Link>
